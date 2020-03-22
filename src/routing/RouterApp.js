085550bea8bfd4e65b4e213877e35d6fe4888889
Aleix Riba <aleix.riba@addcode.es>
2020-03-22 18:08:32 +0100

    implement routing

---
+++ src/routing/RouterApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+export default class RouterApp extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
