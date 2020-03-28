3812a6e59b3c89ea78f8d3667105fd29a5859072
Aleix Riba <aleix.riba@addcode.es>
2020-03-28 13:44:53 +0100

    fix routerApp in merge

--- src/routing/RouterApp.tsx
+++ src/routing/RouterApp.tsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 
+import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
-export default class RouterApp extends Component<{}, {}> {
+export default class RouterApp extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/home" component={Home} />
         </Switch>
       </BrowserRouter>
     );
